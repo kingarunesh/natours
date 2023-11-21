9fe43585288932d5428159f0aa3b79c8b88ebfad
Arunesh kumar <aruneshkumar.king@gmail.com>
2023-11-21 18:40:16 +0530

    8.2 : simple mongoose schema

--- server.js
+++ server.js
@@ -18,6 +18,40 @@ mongoose
     })
     .catch((error) => console.log(error));
 
+//      tour schema
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "A tour must have name"],
+        unique: true,
+    },
+
+    rating: {
+        type: Number,
+        default: 4.5,
+    },
+
+    price: {
+        type: Number,
+        required: [true, "A tour must have price"],
+    },
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
+
+const testTour = new Tour({
+    name: "Jantar Mantra",
+    price: 349,
+});
+
+testTour
+    .save()
+    .then((document) => {
+        console.log(document);
+        console.log("New Document Inserted to Database");
+    })
+    .catch((error) => console.log(`💥ERROR : ${error}`));
+
 //NOTE :        Envirment Variables
 // console.log(app.get("env"));
 // console.log(process.env);
