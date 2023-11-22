0dd0dc23fca127fee610d5e0fe5c354312bfeb4f
Arunesh kumar <aruneshkumar.king@gmail.com>
2023-11-22 19:53:40 +0530

    8.11 : api-features class created for getAllTours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require("../models/tourModel");
+const APIFeatures = require("../utils/apiFeatures");
 
 //SECTION :     middleware
 exports.aliasTopTours = (req, res, next) => {
@@ -20,85 +21,13 @@ exports.top5ByRatingsAverage = (req, res, next) => {
 //SECTION :     get all tours
 exports.getAllTours = async (req, res) => {
     try {
-        //NOTE     get all tours
-        // const tours = await Tour.find();
-
-        //NOTE     direct filtering
-        // const tours = await Tour.find({ duration: 5, difficulty: "easy" });
-
-        // const tours = await Tour.find()
-        //     .where("duration")
-        //     .equals(5)
-        //     .where("difficulty")
-        //     .equals("easy");
-
-        //NOTE     filter object from url
-        // const tours = await Tour.find(req.query);
-
-        //NOTE     filtering tours and exclude others type
-        // const tours = await Tour.find(req.query);
-
-        const queryObj = { ...req.query };
-        const excludesFields = ["page", "limit", "sort", "fields"];
-        excludesFields.forEach((el) => delete queryObj[el]);
-
-        //  for simple filtering
-        // const query = Tour.find(queryObj);
-
-        //NOTE     adv filtering (gte | gt | lte | lt)
-        //  i can use directly but it is not user friendly
-        // 127.0.0.1:5000/api/v1/tours?duration[$gte]=5&difficulty=easy
-
-        let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(
-            /\b(gte|gt|lte|lt)\b/g,
-            (match) => `$${match}`,
-        );
-
-        //  conditionally filtering like gte, gt, lte, lt
-        let query = Tour.find(JSON.parse(queryStr));
-
-        //NOTE :        sort
-        if (req.query.sort) {
-            const sortBy = req.query.sort.split(",").join(" ");
-
-            query = query.sort(sortBy);
-        } else {
-            query = query.sort("-createdAt");
-        }
-
-        //NOTE :    fields limit
-        // 127.0.0.1:5000/api/v1/tours?fields=name,price,images
-        // 127.0.0.1:5000/api/v1/tours?fields=-name,-price
-        if (req.query.fields) {
-            // this will also work
-            // query = query.select("name price");
-
-            const fields = req.query.fields.split(",").join(" ");
-            query = query.select(fields);
-        } else {
-            query = query.select("-__v");
-        }
-
-        //NOTE :    pagination
-        const page = Number(req.query.page) || 1;
-        const limit = Number(req.query.limit) || 10;
-        const skip = (page - 1) * limit;
-
-        query = query.skip(skip).limit(limit);
-
-        if (req.query.page) {
-            const totalDocuments = await Tour.countDocuments();
-            // console.log(countDocuments);
-
-            if (skip >= totalDocuments)
-                throw new Error("Page or Limit is invalid 💥");
-        }
-
-        // console.log(req.query);
-
         //NOTE :    result query
-        const tours = await query;
+        const features = new APIFeatures(Tour.find(), req.query)
+            .filter()
+            .sort()
+            .limitFields()
+            .pagination();
+        const tours = await features.query;
 
         //NOTE     send response to user
         res.status(200).json({
