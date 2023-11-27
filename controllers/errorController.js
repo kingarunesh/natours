882bb9909e04f5ad3dd4890bff496214f7a7d4a0
Arunesh kumar <aruneshkumar.king@gmail.com>
2023-11-27 07:20:15 +0530

    9.6 : dev VS prod ERROR

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,35 @@
+const sendErrorDev = (error, res) => {
+    res.status(error.statusCode).json({
+        status: error.status,
+        message: error.message,
+        stack: error.stack,
+        error: error,
+    });
+};
+
+const sendErrorProd = (error, res) => {
+    if (error.isOperational) {
+        res.status(error.statusCode).json({
+            status: error.status,
+            message: error.message,
+        });
+    } else {
+        console.error("💥ERROR : ", error);
+
+        res.status(500).json({
+            status: "error",
+            message: "something went wrong",
+        });
+    }
+};
+
 module.exports = (error, req, res, next) => {
     error.statusCode = error.statusCode || 500;
     error.status = error.status || "error";
 
-    res.status(error.statusCode).json({
-        status: error.status,
-        message: error.message,
-    });
+    if (process.env.NODE_DEV === "development") {
+        sendErrorDev(error, res);
+    } else if (process.env.NODE_DEV === "production") {
+        sendErrorProd(error, res);
+    }
 };
