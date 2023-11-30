2d2b18cf2e3670ba6dc19c1518d70a60781c373d
Arunesh kumar <aruneshkumar.king@gmail.com>
2023-11-30 12:43:11 +0530

    10.13 : jwt cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,6 +16,22 @@ const tokenSign = (id) => {
 const createSendToken = (user, statusCode, res) => {
     const token = tokenSign(user._id);
 
+    const cookieOptions = {
+        expries: new Date(
+            Date.now() +
+                process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+        ),
+
+        httpOnly: true,
+    };
+
+    if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+
+    res.cookie("jwt", token, cookieOptions);
+
+    //!     remove password password
+    user.password = undefined;
+
     res.status(statusCode).json({
         status: "success",
         token,
